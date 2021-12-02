42ee55ed3ba23241688b16ff6242ade5c76b42b6
fatanabil <fatanabil63@gmail.com>
2021-12-02 17:11:15 +0700

    edit service worker

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ let staticCacheName = "pwa";
 self.addEventListener("install", function (e) {
     e.waitUntil(
         caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll(["/"]);
+            return cache.addAll(["./"]);
         })
     );
 });
